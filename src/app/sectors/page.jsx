93e070e56c1dda228cecd74c0741e52268a709db
RENTKAR <aman@beeyond.agency>
2025-02-04 12:57:23 +0530

    fix

--- src/app/sectors/page.jsx
+++ src/app/sectors/page.jsx
@@ -325,7 +325,7 @@ const imageRefs = useRef([]);
              <div className=' h-[100%] md:h-[100%] pb-4 md:pb-0  bg-[#02123b]'>
                  <div className={`md:pl-[2vw] lg:pl-[4vw] px-4 md:px-0 flex flex-col md:flex-row h-[100%] gap-6 md:gap-0 justify-end md:justify-between md:items-center text-white ${Clash.className}  `}>
                      <div className=' text-[10vw] lg:text-[6vw] md:text-[8vw] h-auto md:h-[50%] uppercase'>Sector expertise</div>
-                     <div className='text-base pr-[8vw]  md:w-[48%] md:text-[1.7vw] lg:text-[1.3vw] h-auto md:h-[50%] flex items-end'>From pharmaceuticals to hazardous goods, explore our specialized logistics expertise designed to meet the unique demands of diverse industries with precision, safety, and reliability.</div>
+                     <div className='text-base pr-[8vw]  md:w-[48%] md:text-[1.7vw] lg:text-[1.4vw] h-auto md:h-[50%] flex items-end'>From pharmaceuticals to hazardous goods, explore our specialized logistics expertise designed to meet the unique demands of diverse industries with precision, safety, and reliability.</div>
                  </div>
              </div>
          </div>
